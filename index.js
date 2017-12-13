5b7108852b728ccde69215d5120205acf7223977
Dave Staab <davestaab@gmail.com>
2017-12-13 09:56:54 -0500

    adding .editorconfig file and reformatting to match original format preferences

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 // Uses Node, AMD or browser globals to create a module.
 
 // If you want something that will work in other stricter CommonJS environments,
@@ -16,52 +14,52 @@
 // can remove the `root` use and the passing `this` as the first arg to
 // the top function.
 
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-      // AMD. Register as an anonymous module.
-      define(['angular'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-      // Node. Does not work with strict CommonJS, but
-      // only CommonJS-like environments that support module.exports,
-      // like Node.
-      module.exports = factory(require('angular'));
-  } else {
-      // Browser globals (root is window)
-      root.returnExports = factory(root.angular);
-  }
-}(typeof self !== 'undefined' ? self : this, function (angular) {
-  return angular.module('isolateForm', []).directive('isolateForm', [function () {
-    return {
-      restrict: 'A',
-      require: '?form',
-      link: function (scope, elm, attrs, ctrl) {
-        if (!ctrl) {
-          return;
-        }
-  
-        // Do a copy of the controller
-        var ctrlCopy = {};
-        angular.copy(ctrl, ctrlCopy);
-  
-        // Get the parent of the form
-        var parent = elm.parent().controller('form');
-        // Remove parent link to the controller
-        parent.$removeControl(ctrl);
-  
-        // Replace form controller with a "isolated form"
-        var isolatedFormCtrl = {
-          $setValidity: function (validationToken, isValid, control) {
-            ctrlCopy.$setValidity(validationToken, isValid, control);
-            parent.$setValidity(validationToken, true, ctrl);
-          },
-          $setDirty: function () {
-            elm.removeClass('ng-pristine').addClass('ng-dirty');
-            ctrl.$dirty = true;
-            ctrl.$pristine = false;
-          },
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['angular'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('angular'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.angular);
+    }
+}(typeof self !== 'undefined' ? self : this, function(angular) {
+    return angular.module('isolateForm', []).directive('isolateForm', [function() {
+        return {
+            restrict: 'A',
+            require: '?form',
+            link: function(scope, elm, attrs, ctrl) {
+                if (!ctrl) {
+                    return;
+                }
+
+                // Do a copy of the controller
+                var ctrlCopy = {};
+                angular.copy(ctrl, ctrlCopy);
+
+                // Get the parent of the form
+                var parent = elm.parent().controller('form');
+                // Remove parent link to the controller
+                parent.$removeControl(ctrl);
+
+                // Replace form controller with a "isolated form"
+                var isolatedFormCtrl = {
+                    $setValidity: function(validationToken, isValid, control) {
+                        ctrlCopy.$setValidity(validationToken, isValid, control);
+                        parent.$setValidity(validationToken, true, ctrl);
+                    },
+                    $setDirty: function() {
+                        elm.removeClass('ng-pristine').addClass('ng-dirty');
+                        ctrl.$dirty = true;
+                        ctrl.$pristine = false;
+                    }
+                };
+            angular.extend(ctrl, isolatedFormCtrl);
+            }
         };
-        angular.extend(ctrl, isolatedFormCtrl);
-      }
-    };
-  }]).name;
+    }]).name;
 }));
